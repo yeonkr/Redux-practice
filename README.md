a19a63c75725146751b9bda9ce6ce82809df44cd
yeonkr <1714882@sookmyung.ac.kr>
2022-11-24 00:14:55 +0900

    feat: switch & case

--- README.md
+++ README.md
@@ -13,3 +13,20 @@
 ✅ Reducer에게 Action을 보내는 방법 : store.dispatch({key: value});
 ✅ Subscribe : store 안에 있는 변화 감지
 store.subscribe(func); // store안의 변화를 감지하면 func 실행
+
+< Recap >
+✅ reducer : 현재 상태의 application과 함께 불려지는 function (+ with action)
+return하는 것은 application의 state가 됨
+✅ action : reducer와 소통하는 방법으로 Object여야 하며 그 key 이름은 항상 type임 (바꿀 수 없음)
+✅ dispatch : reducer에게 action을 보내는 방법
+✅ subscribe : store의 변화를 감지하면 인자값으로 준 함수를 실행
+✅ if (action.type==='sadfsa') 대신 switch가 자주 쓰임
+switch(action.type){
+case ..blah..:
+return smth
+case ..blah2..:
+return smth2
+default:
+return smth3
+}
+✅ string으로 바로 쓰는 대신에 const variable로 선언해서 사용하기 -> 에러 발견 용이
--- src/index.js
+++ src/index.js
@@ -4,18 +4,26 @@ const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
+number.innerText = 0;
+
+// 오타 방지용 action
+const ADD = "ADD";
+const MINUS = "MINUS";
+
 const countModifier = (count = 0, action) => {
-  console.log(count, action);
-  if (action.type === "ADD") {
-    return count + 1;
-  } else if (action.type === "MINUS") {
-    return count - 1;
-  } else {
-    return count;
+  // countModifier() : 데이터를 수정하고, 이 함수가 리턴하는 건 '새로운 데이터'다.
+  // 액션을 보낼때마다 액션을 디스패치할때마다 이 함수는 current state에 의해 불러지고 count 증감될 것.
+
+  // if else 대신 switch case
+  switch (action.type) {
+    case ADD:
+      return count + 1;
+    case MINUS:
+      return count - 1;
+    default:
+      return count;
   }
 };
-
-number.innerText = 0;
 
 const countStore = createStore(countModifier);
 
@@ -26,11 +34,11 @@ const onChange = () => {
 countStore.subscribe(onChange);
 
 const handleAdd = () => {
-  countStore.dispatch({ type: "ADD" });
+  countStore.dispatch({ type: ADD });
 };
 
 const handleMinus = () => {
-  countStore.dispatch({ type: "MINUS" });
+  countStore.dispatch({ type: MINUS });
 };
 
 add.addEventListener("click", handleAdd);
