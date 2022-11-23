b04e3af02ac1b8102471e4343936c990d971e5b3
yeonkr <1714882@sookmyung.ac.kr>
2022-11-24 00:40:51 +0900

    feat: todo

--- src/index.js
+++ src/index.js
@@ -73,3 +73,33 @@ minus.addEventListener("click", handleMinus);
 
 // add.addEventListener("click", handleAdd);
 // minus.addEventListener("click", handleMinus);
+
+/* TODO */
+const form = document.querySelector("form");
+const input = document.querySelector("input");
+const ul = document.querySelector("ul");
+const ADD_TODO = "ADD_TODO";
+const DELETE_TODO = "DELETE_TODO";
+
+const reducer = (state = [], action) => {
+  switch (action.type) {
+    case ADD_TODO:
+      return [...state, { text: action.text, id: Date.now() }];
+    case DELETE_TODO:
+      return [];
+    default:
+      return state;
+  }
+};
+
+const store = createStore(reducer);
+
+store.subscribe(() => console.log(store.getState()));
+
+const onSubmit = (e) => {
+  e.preventDefault();
+  const toDo = input.value;
+  input.value = "";
+  store.dispatch({ type: ADD_TODO, text: toDo });
+};
+form.addEventListener("submit", onSubmit);
