fb437c69ccdba23ee1d7fed9c495f78a948f677a
yeonkr <1714882@sookmyung.ac.kr>
2022-11-24 00:04:23 +0900

    feat: Subscribe()

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # Redux Practice
+
+## Redux
+
+- 기본적으로 Javascript application들의 state(상태)를 관리하는 방법
+  - React와 많이 사용하면서 유명해졌으나, React에 의존하는 라이브러리는 아님.
+  - 이와는 별개로 Angular, Vue.js, Vanilla JS 등 JS 언어내의 여러곳에서 사용가능함.
+
+✅ Store는 data를 저장하는 곳
+✅ CreateStore는 reducer를 요구함.
+✅ Reducer는 data를 modify 해주는 함수로 reducer가 return하는 것은 application에 있는 data가 됨.
+✅ Action : redux에서 function을 부를 때 쓰는 두 번째 parameter 혹은 argument으로 reducer와 소통하기 위한 방법
+✅ Reducer에게 Action을 보내는 방법 : store.dispatch({key: value});
+✅ Subscribe : store 안에 있는 변화 감지
+store.subscribe(func); // store안의 변화를 감지하면 func 실행
--- src/index.js
+++ src/index.js
@@ -1,24 +1,67 @@
+import { createStore } from "redux";
+
 const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
-let count = 0;
-
-number.innerText = count;
-
-const updateText = () => {
-  number.innerText = count;
+const countModifier = (count = 0, action) => {
+  console.log(count, action);
+  if (action.type === "ADD") {
+    return count + 1;
+  } else if (action.type === "MINUS") {
+    return count - 1;
+  } else {
+    return count;
+  }
 };
 
+number.innerText = 0;
+
+const countStore = createStore(countModifier);
+
+const onChange = () => {
+  number.innerText = countStore.getState();
+};
+
+countStore.subscribe(onChange);
+
 const handleAdd = () => {
-  count++;
-  updateText();
+  countStore.dispatch({ type: "ADD" });
 };
 
 const handleMinus = () => {
-  count--;
-  updateText();
+  countStore.dispatch({ type: "MINUS" });
 };
 
 add.addEventListener("click", handleAdd);
 minus.addEventListener("click", handleMinus);
+
+//--------------------------------------//
+
+// countStore.dispatch({ type: "ADD" }); // 액션은 객체로.
+// // 메시지를 보낼 땐 디스패치를 사용한다.
+// // 내가 전송한 메세지는 액션에 넣고, 해야할 일은 액션을 체크하면 됨
+
+// countStore.dispatch({ type: "MINUS" });
+
+// console.log(countStore.getState());
+// let count = 0;
+
+// number.innerText = count;
+
+// const updateText = () => {
+//   number.innerText = count;
+// };
+
+// const handleAdd = () => {
+//   count++;
+//   updateText();
+// };
+
+// const handleMinus = () => {
+//   count--;
+//   updateText();
+// };
+
+// add.addEventListener("click", handleAdd);
+// minus.addEventListener("click", handleMinus);
