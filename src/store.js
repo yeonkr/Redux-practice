fc7a3f9f10c3ad9c31dacf9042993cb35d32f9fc
yeonkr <1714882@sookmyung.ac.kr>
2022-11-27 00:03:21 +0900

    feat: createAction

--- src/store.js
+++ src/store.js
@@ -1,39 +1,86 @@
+// // 리덕스
+// // import { createStore } from "redux";
+// // const ADD = "ADD";
+// // const DELETE = "DELETE";
+
+// // export const addToDo = (text) => {
+// //   return {
+// //     type: ADD,
+// //     text,
+// //   };
+// // };
+
+// // export const deleteToDo = (id) => {
+// //   return {
+// //     type: DELETE,
+// //     id,
+// //   };
+// // };
+
+// // const reducer = (
+// //   state = JSON.parse(localStorage.getItem("todos")) || [],
+// //   action
+// // ) => {
+// //   switch (action.type) {
+// //     case ADD:
+// //       return save([{ text: action.text, id: Date.now() }, ...state]);
+// //     case DELETE:
+// //       return save(state.filter((toDo) => toDo.id !== action.id));
+// //     default:
+// //       return state;
+// //   }
+// // };
+
+// // const save = (state) => {
+// //   localStorage.setItem("todos", JSON.stringify(state));
+// //   return state;
+// // };
+
+// // const store = createStore(reducer);
+
+// // export default store;
+
+// // 툴킷
+// import { createStore } from "redux";
+// import { createAction } from "@reduxjs/toolkit";
+
+// const addToDo = createAction("ADD");
+// const deleteToDo = createAction("DELETE");
+
+// const reducer = (state = [], action) => {
+//   switch (action.type) {
+//     case addToDo.type:
+//       return [{ text: action.payload, id: Date.now() }, ...state];
+//     case deleteToDo.type:
+//       return state.filter((toDo) => toDo.id !== action.payload);
+//     default:
+//       return state;
+//   }
+// };
+// const store = createStore(reducer);
+// export const actionCreators = {
+//   addToDo,
+//   deleteToDo,
+// };
+// export default store;
+
+// 툴킷
 import { createStore } from "redux";
-const ADD = "ADD";
-const DELETE = "DELETE";
+import { createAction } from "@reduxjs/toolkit";
 
-export const addToDo = (text) => {
-  return {
-    type: ADD,
-    text,
-  };
-};
+export const addToDo = createAction("ADD");
+export const deleteToDo = createAction("DELETE");
 
-export const deleteToDo = (id) => {
-  return {
-    type: DELETE,
-    id,
-  };
-};
-const reducer = (
-  state = JSON.parse(localStorage.getItem("todos")) || [],
-  action
-) => {
+const reducer = (state = [], action) => {
   switch (action.type) {
-    case ADD:
-      return save([{ text: action.text, id: Date.now() }, ...state]);
-    case DELETE:
-      return save(state.filter((toDo) => toDo.id !== action.id));
+    case addToDo.type:
+      return [{ text: action.payload, id: Date.now() }, ...state];
+    case deleteToDo.type:
+      return state.filter((toDo) => toDo.id !== action.payload);
     default:
       return state;
   }
 };
-
-const save = (state) => {
-  localStorage.setItem("todos", JSON.stringify(state));
-  return state;
-};
-
 const store = createStore(reducer);
 
 export default store;
